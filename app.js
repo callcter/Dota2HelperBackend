72dbf01eb7c436486a40f35cb0eb3cb8cb647d7c
刘杭 <callcter@diancangstudio.com>
2016-10-25 18:18:34 +0800

    route fixed

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 var express = require('express');
-var app = express();
-var routes = require('./routes')(app);
+var path = require('path');
+var logger = require('morgan');
+var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
 
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var apis = require('./routes/apis');
+
+var app = express();
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','jade');
+
+app.use(favicon(path.join(__dirname+'/public/favicon.ico')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/',routes);
+app.use('/users',users);
+app.use('/apis',apis);
+
+//上传时修改端口号为3000
 app.listen(3000);
